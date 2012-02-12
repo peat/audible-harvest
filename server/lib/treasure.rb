74ab1118508057fb950371863d1d5489a2d32e69
Peat Bakke <peat@peat.org>
2012-02-12 06:23:01 -0800

    Stylin

--- server/lib/treasure.rb
+++ server/lib/treasure.rb
@@ -1,5 +1,14 @@
 class Treasure < ActiveRecord::Base
-  def pretty_print
-    [ id, created_at, person, track, artist, provider, origin ].join(" :: ")
+
+  def self.provider_data
+    providers = {}
+
+    self.all.each do |p|
+      providers[p] ||= 0
+      providers[p] += 1
+    end
+
+    providers.keys.collect { |k| [k, providers[k]] }.to_json
   end
+
 end
