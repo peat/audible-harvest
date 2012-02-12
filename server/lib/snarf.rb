469d2b9663431e325deaea51dfed084d42e03a31
Peat Bakke <peat@peat.org>
2012-02-11 19:24:59 -0800

    Plumbing in Treasure!

--- server/lib/snarf.rb
+++ server/lib/snarf.rb
@@ -11,13 +11,18 @@ class MHDApp
   post '/snarf' do
     content_type :json
 
-    person = params['person']
-    track = params['track']
-    artist = params['artist']
-    provider = params['provider']
-    origin = params['origin']
+    record = {
+      :person => params['person'],
+      :track => params['track'],
+      :artist => params['artist'],
+      :provider => params['provider'],
+      :origin => params['origin']
+    }
 
-    { :person => person, :track => track, :provider => provider, :origin => origin }.to_json
+    treasure = Treasure.new( record )
+    treasure.save
+
+    record.to_json
   end
 
 end
---
+++ server/lib/treasure.rb
@@ -0,0 +1,3 @@
+class Treasure < ActiveRecord::Base
+  # derp
+end
