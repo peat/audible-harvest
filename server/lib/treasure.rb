c72a285b4870dc65f747c44f23cb8c083a8b5309
Peat Bakke <peat@peat.org>
2012-02-12 06:38:21 -0800

    Fixing up people chart

--- server/lib/treasure.rb
+++ server/lib/treasure.rb
@@ -22,4 +22,16 @@ class Treasure < ActiveRecord::Base
     origins.keys.collect { |k| [k, origins[k]] }.sort { |a,b| b[1] <=> a[1] }.to_json
   end
 
+  def self.people_data
+    people = {}
+
+    self.all.each do |t|
+      people[t.people] ||= 0
+      people[t.people] += 1
+    end
+
+    # only return the top 5 ...
+    people.keys.collect { |k| [k, people[k]] }.sort { |a,b| b[1] <=> a[1] }.slice(0,5).to_json
+  end
+
 end
